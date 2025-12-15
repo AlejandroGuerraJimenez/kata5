6ac5a9acb4227177212b266c6ca29ca8fbf5503c
AlejandroGuerraJimenez <alejandro.guerra111@alu.ulpgc.es>
2025-12-15 23:41:33 +0000

    Created and Implemented class DatabaseStore

--- src/main/java/software/ulpgc/kata5/architecture/io/Store.java
+++ src/main/java/software/ulpgc/kata5/architecture/io/Store.java
@@ -2,8 +2,9 @@ package software.ulpgc.kata5.architecture.io;
 
 import software.ulpgc.kata5.architecture.Model.Movie;
 
+import java.sql.SQLException;
 import java.util.stream.Stream;
 
 public interface Store {
-    Stream<Movie> movies();
+    Stream<Movie> movies() throws SQLException;
 }
